3d136ee639e3e9e72edc9077d5ce6a0ac544bf21
Vigan Shemsiu <vigan@shemsiu.dk>
2016-02-23 23:01:54 +0100

    Start implementing of newsletter

---
+++ app/Http/Controllers/NewsletterController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class NewsletterController extends Controller
+{
+    //Signup for newsletter
+    //Remove from the mailing list (only change the active field from true to false)
+    //Send newsletter to the mailing list where "active" is true
+    //More coming
+}
