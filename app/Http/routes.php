a03a3dd00f6de3d0ab994394ca55013ed53e2bc6
Vigan Shemsiu <vigan@shemsiu.dk>
2016-02-24 21:44:23 +0100

    Modify routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -8,9 +8,14 @@ Route::get('frontpage', function () {
     return view('pages.index');
 });
 
+Route::get('map', function () {
+    return view('pages.map');
+});
+
+
 Route::post('/subscribe', 'NewsletterController@subscribe');
 Route::get('/unsubscribe/{email}', 'NewsletterController@unsubscribe');
 
-Route::get('map', function () {
-    return view('pages.map');
+Route::group(['prefix' => 'api'], function() {
+   Route::get('mail-notify', 'ReminderController@daily_notify');
 });
