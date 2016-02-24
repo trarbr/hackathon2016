9afde8d769c11842f545eae39fa7d4e6157e13fa
Vigan Shemsiu <vigan@shemsiu.dk>
2016-02-24 09:26:47 +0100

    newsletter request

--- app/Http/Controllers/NewsletterController.php
+++ app/Http/Controllers/NewsletterController.php
@@ -9,6 +9,25 @@ use App\Http\Controllers\Controller;
 
 class NewsletterController extends Controller
 {
+    public function __construct()
+    {
+
+    }
+
+    public function create()
+    {
+        
+    }
+
+    public function signup()
+    {
+        
+    }
+
+    public function deactivate()
+    {
+
+    }
     //Signup for newsletter
     //Remove from the mailing list (only change the active field from true to false)
     //Send newsletter to the mailing list where "active" is true
