322865f3a030c10d589c1d2422768130e1a1e42f
Vigan Shemsiu <vigan@shemsiu.dk>
2016-02-24 12:01:17 +0100

    Making subscribe and unsubscribe work!

--- app/Http/Controllers/NewsletterController.php
+++ app/Http/Controllers/NewsletterController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\NewsletterRequest;
+use App\News;
 use App\Newsletter;
 use Illuminate\Database\QueryException;
 use Illuminate\Http\Request;
@@ -20,28 +21,44 @@ class NewsletterController extends Controller
 
     public function subscribe(Request $request)
     {
-        try {
-            Newsletter::create([
-                'email' => $request->email,
-                'street' => $request->street,
-                'zipcode' => $request->zipcode,
-                'mobile' => $request->mobile,
-                'notification_type' => $request->notification_type
-            ]);
-            return "Fint!";
-
-        } catch (\Exception $e) {
-            return "Der er opstået en fejl!";
+        $user = Newsletter::where('email', $request->email)->first();
+        if ($user == null) {
+            try {
+                Newsletter::create([
+                    'email' => $request->email,
+                    'street' => $request->street,
+                    'zipcode' => $request->zipcode,
+                    'mobile' => $request->mobile,
+                    'notification_type' => $request->notification_type
+                ]);
+                return "Fint!";
+            } catch (\Exception $e) {
+                return "Der er opstået en fejl!";
+            }
+        } else {
+            return $this->subscribe_again($request);
         }
     }
 
     public function unsubscribe($email)
     {
-        $user = Newsletter::where('email', $email)->get();
-        if ($user != null) {
-            $user->active = false;
-            $user->save();
-        }
+        $user = Newsletter::where('email', $email)->first();
+        $user->active = false;
+        $user->save();
+    }
+
+    private function subscribe_again($request)
+    {
+        $user = Newsletter::where('email', $request->email)->first();
+
+        $user->email = $request->email;
+        $user->street = $request->street;
+        $user->zipcode = $request->zipcode;
+        $user->mobile = $request->mobile;
+        $user->notification_type = $request->notification_type;
+        $user->active = true;
+
+        $user->save();
     }
     //Remove from the mailing list (only change the active field from true to false)
     //Send newsletter to the mailing list where "active" is true
